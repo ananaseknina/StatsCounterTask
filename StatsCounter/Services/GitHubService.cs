2c3e3ec66dad6168256452d0be5188674e9b1cf0
Nina Marszalek <Nina.Marszalek@gds.ey.com>
2023-04-07 17:50:00 +0200

    missing one

--- StatsCounter/Services/GitHubService.cs
+++ StatsCounter/Services/GitHubService.cs
@@ -1,8 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Threading.Tasks;
-using Octokit;
 using StatsCounter.Contracts;
 using StatsCounter.Models;
 
@@ -15,15 +16,33 @@ namespace StatsCounter.Services
         public GitHubService(HttpClient httpClient)
         {
             _httpClient = httpClient;
-
         }
 
         public async Task<IEnumerable<RepositoryInfo>> GetRepositoryInfosByOwnerAsync(string owner)
         {
-            string accessToken = "";
-            _httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
-            var data = (await _httpClient.GetStringAsync($"https://api.github.com/users/{owner}/repos"));
-            var repositoryInfos = JsonSerializer.Deserialize<List<RepositoryInfo>>(data);
+            var repositoryInfos = new List<RepositoryInfo>();
+            try
+            {
+                var url = $"https://api.github.com/users/{owner}/repos";
+                var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+                var productValue = new ProductInfoHeaderValue("StatsCounter", "1.0");
+                var commentValue = new ProductInfoHeaderValue("(+http://www.API.com/StatsCounter.html)");
+
+                request.Headers.UserAgent.Add(productValue);
+                request.Headers.UserAgent.Add(commentValue);
+
+                _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                
+                var content = (await _httpClient.SendAsync(request)).Content;
+                var data = await content.ReadAsStringAsync();
+                repositoryInfos = JsonSerializer.Deserialize<List<RepositoryInfo>>(data);
+            }
+            catch(Exception e) 
+            {
+                throw new ArgumentException("skksskks"+e.Message);
+            }
+            
             return repositoryInfos;
         }
     }
